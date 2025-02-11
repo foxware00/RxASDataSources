3f641bd660456ded155604375a471a9b6a085fcc
Oliver Fox <fox.oliverj@gmail.com>
2025-02-11 20:52:23 +0000

    Update pod

--- Package.swift
+++ Package.swift
@@ -13,11 +13,12 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
     .package(url: "https://github.com/shimastripe/Texture.git", .upToNextMajor(from: "3.1.1"))
+    .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "5.0.0"))
   ],
   targets: [
     .target(name: "RxASDataSources",
             dependencies: [
-              .product(name: "Differentiator", package: "RxSwift"),
+              .product(name: "Differentiator", package: "RxDataSources"),
               .product(name: "RxSwift", package: "RxSwift"),
               .product(name: "RxCocoa", package: "RxSwift"),
               "Texture"
