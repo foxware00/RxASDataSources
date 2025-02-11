a3d1fb72a6616de8885d0df0b653acdb3bfede22
Oliver Fox <fox.oliverj@gmail.com>
2025-02-11 18:21:02 +0000

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+  name: "RxASDataSources",
+  platforms: [
+    .iOS(.v9), .tvOS(.v9)
+  ],
+  products: [
+    .library(name: "RxASDataSources", targets: ["RxASDataSources"]),
+    .library(name: "Differentiator", targets: ["Differentiator"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+    .package(url: "https://github.com/shimastripe/Texture.git", .upToNextMajor(from: "3.1.1"))
+  ],
+  targets: [
+    .target(name: "RxASDataSources", dependencies: ["Differentiator", "RxSwift", "RxCocoa"]),
+    .target(name: "Differentiator"),
+    .testTarget(name: "RxASDataSourcesTests", dependencies: ["RxASDataSources"])
+  ],
+  swiftLanguageVersions: [.v5]
+)
